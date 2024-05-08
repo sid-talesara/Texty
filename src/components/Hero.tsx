f227aff4d6010564ed428b9f380510e9ac7fe2a2
sid-talesara <2020pcecssiddharth207@poornima.org>
2024-05-08 23:13:53 +0530

    Fix: Resolved Production issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,7 @@
-import { FaPenAlt } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className="pt-36">
       <nav className="fixed text-black top-0 left-0  bg-gray-400 w-96 mx-auto rounded-full h-16 flex items-center  mt-4 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100 right-0 p-4 text-3xl gap-3 text-center  justify-center font-Montserrat">
-        {/* <FaPenAlt />  */}
         TEXTY
       </nav>
 
